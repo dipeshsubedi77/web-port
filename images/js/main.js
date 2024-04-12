4f3c36ae6d06da6d26f992a999975078bc63e7ff
dipesh subedi <dipeshfitness74@gmail.com>
2024-04-12 14:31:25 +0545

    hello

--- images/js/main.js
+++ images/js/main.js
@@ -166,3 +166,16 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== header counter==============*/
+
+const sr = ScrollReveal({
+  origin:'top',
+  distance:'60px',
+  duration:2500,
+  delay:400,
+
+})
+sr.reveal('.home__data, .projects__container, .services__container, .testimonial__container, .footer__container')
+sr.reveal('.home__info div', {delay: 600, origin: 'bottom', interval: 100})
+sr.reveal('.skills__content:nth-child(1), contact__content:nth-child(1)', { origin: 'left'})
+sr.reveal('.skills__content:nth-child(2), contact__content:nth-child(2)', { origin: 'right'})
+sr.reveal('.qualification__content,services__card', {interval:100})
